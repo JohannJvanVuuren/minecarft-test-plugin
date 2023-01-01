02142836b2a1c17536d3cae9aa451251b7f262ae
Johann JvanVuuren <technomagekomt@gmail.com>
2023-01-01 10:05:43 +0200

    Initial commit.

---
+++ src/main/java/io/codepoint/mcplugin/MCplugin.java
@@ -0,0 +1,17 @@
+package io.codepoint.mcplugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCplugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
